8d1922fa9f7c6f06a74443cd8182dd4a300183e4
qraees <107559816+qraees@users.noreply.github.com>
2022-09-15 21:21:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+//all elemants
+
+
+const username =document.querySelector("#username");
+const email =document.querySelector("#email");
+const password =document.querySelector("#password");
+const confirmPassword =document.querySelector("#confirmPassword");
+const form =document.querySelector("#form")
+
+     const showError =(input,massage) => {
+         
+     let parentElement =input.parentElement;
+     parentElement.classList = 'form-control error'; 
+     const small =parentelement.querySelector("small")
+     const successIcon = parentElement.queryselectorAll("i")
+     [0];
+     const errorIcon = parentElement.queryselectorAll("i")
+     [1];
+     errorIcon.style.visibility = 'visible';
+     successIcon.style.visibility = 'hidden';
+     small.innerText = massage;
+     
+       
+     }
+     
+     const showSuccess =(input) => {
+         
+     let parentElement =input.parentElement;
+     parentElement.classList = 'form-control success'; 
+     const successIcon = parentElement.queryselectorAll("i")
+     [0];
+     const errorIcon = parentElement.queryselectorAll("i")
+     [1];
+     errorIcon.style.visibility = 'hidden';
+     successIcon.style.visibility = 'visible';
+     
+
+const ckeckEmpty = (element) =>{
+    
+    elemants.forEach( (elemant) =>{
+    if (elements.value ==='') { 
+      showError(element,'input required');
+    }else{
+        showSUCESS(element);
+    }
+    });
+}
+
+form.addEventListener("#submit",(event)=>{
+    
+    event.preventDefault();
+   
+   checkEmpty( [username,email,password,confirmPassword]);
+})
